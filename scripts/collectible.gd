08aa7c98cb51b6928fac9055cfd73f5443d49d9e
MATHIS\vertm <vert.mathis@gmail.com>
2025-04-21 12:42:08 +0200

    added collectible respawn logic

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -4,6 +4,15 @@ extends Node2D
 
 @export var ingredient: Ingredient
 
+func collect() -> void:
+	GlobalInventory.add_ingredient(ingredient)
+	self.hide()
+	$RespawnTimer.start()
+
+
 func _on_area_2d_body_entered(body):
-	if body.name == "Bird":
-		GlobalInventory.add_ingredient(ingredient)
+	if body.name == "Bird" and $RespawnTimer.is_stopped():
+		collect()
+
+func _on_respawn_timer_timeout():
+	self.show()
