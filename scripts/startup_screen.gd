3d3af353790d3dc92b8f2b1fb117235a02e1c1ec
clemz <clementpouget@gmail.com>
2025-04-21 19:23:53 +0200

    restore

--- scripts/startup_screen.gd
+++ scripts/startup_screen.gd
@@ -3,8 +3,10 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
+	%Sounds/ButtonClick.play()
 	get_tree().get_root().get_node("Main").start_game()
 
 
 func _on_tutorial_button_pressed() -> void:
+	%Sounds/ButtonClick.play()
 	get_tree().get_root().get_node("Main").show_tutorial()
--- scripts/tutorial_screen.gd
+++ scripts/tutorial_screen.gd
@@ -2,4 +2,5 @@ extends Control
 
 
 func _on_return_to_startup_screen_pressed() -> void:
+	%Sounds/ButtonClick.play()
 	get_tree().get_root().get_node("Main").hide_tutorial()
