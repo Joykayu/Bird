1f8d2c4673d69e77c726e8915f4642e422defe22
MATHIS\vertm <vert.mathis@gmail.com>
2025-04-21 20:38:25 +0200

    last commit

--- scripts/game_over_screen.gd
+++ scripts/game_over_screen.gd
@@ -1,8 +1,7 @@
 extends Control
 
-func _on_submit_high_score_button_pressed() -> void:
-	%Sounds/ButtonClick.play()
-	get_tree().get_root().get_node("Main").submit_high_score()
+func _process(delta):
+	%YourScore.text = str("Score : ",int(floor(GlobalInventory.score)))
 
 func _on_startup_button_pressed() -> void:
 	%Sounds/ButtonClick.play()
